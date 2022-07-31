95789e61e58d6c273ad931df63f59759385a26cb
everton-lacerda <everton_ml@outlook.com>
2022-07-30 21:05:40 -0300

    add typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
 
-export default function Home() {
+type props = {
+  title: string;
+};
+
+export default function Home({ title = "Meu Titulo" }: props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,17 +17,17 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js Truta!</a>
+          {title}
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2>  {title} &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
@@ -58,12 +62,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
