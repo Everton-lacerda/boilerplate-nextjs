ffc1152e3e95ab7e491687f96d680b31e83b2283
everton-lacerda <everton_ml@outlook.com>
2022-07-30 21:31:37 -0300

    add prettier and config eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../../styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
 
 type props = {
-  title: string;
-};
+  title: string
+}
 
-export default function Home({ title = "Meu Titulo" }: props) {
+export default function Home({ title = 'Meu Titulo' }: props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -19,7 +19,7 @@ export default function Home({ title = "Meu Titulo" }: props) {
         <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
+          Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -60,12 +60,12 @@ export default function Home({ title = "Meu Titulo" }: props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  );
+  )
 }
